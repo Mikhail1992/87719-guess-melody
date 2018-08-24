850f196b85e1a1ff5c50aad53fa5dda373711b1b
mikhail_dziarkach <misderkach@yandex.ru>
2018-08-24 23:17:09 +0300

    Добавляет новые функции и тесты к ним

---
+++ js/models/calculate-lives.js
@@ -0,0 +1,16 @@
+const calculateLives = (game, lives) => {
+  if (typeof lives !== `number`) {
+    throw new Error(`Lives should be of type number`);
+  }
+
+  if (lives > game.lives) {
+    throw new Error(`Lives should not be more then initial value`);
+  }
+
+  const newLives = game.lives - lives;
+  const newGame = Object.assign({}, game, {
+    lives: newLives
+  });
+  return newGame;
+};
+export default calculateLives;
---
+++ js/models/checkout-level.js
@@ -0,0 +1,21 @@
+export const INITIAL_GAME = Object.freeze({
+  level: 0,
+  lives: 3,
+  time: 100
+});
+
+const checkoutLevel = (game, level) => {
+  if (typeof level !== `number`) {
+    throw new Error(`Level should be of type number`);
+  }
+
+  if (level < 0) {
+    throw new Error(`Level should not be negative value`);
+  }
+
+  const newGame = Object.assign({}, game, {
+    level
+  });
+  return newGame;
+};
+export default checkoutLevel;
